4b08aac8297d74c925e51eaf4539b071d6444b02
Yusi <yusi4781@naver.com>
2025-03-26 13:35:17 +0900

    delete: 인증 url alert 삭제

--- src/api/Auth/usePwCode.ts
+++ src/api/Auth/usePwCode.ts
@@ -10,10 +10,9 @@ const usePwCode = () => {
 
   const sendEmail = async (email: string): Promise<void> => {
     try {
-      const response = await axios.post<LoginResponse>(`${apiUrl}/auth/password-reset`, {
+      await axios.post<LoginResponse>(`${apiUrl}/auth/password-reset`, {
         email,
       });
-      alert(response.data || "이메일 전송 요청이 성공적으로 처리되었습니다.");
     } catch (error) {
       const message = axios.isAxiosError(error) ? error.response?.data?.message : "이메일 발송 실패";
       alert(message);
